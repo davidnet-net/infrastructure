09929c78d7364f9a1975d6724e3fca66a067d4e4
dedestem <david@davidnet.net>
2025-10-18 15:17:15 +0200

    Fix the EFI partition

--- hosts/asuslaptop/disko-config.nix
+++ hosts/asuslaptop/disko-config.nix
@@ -11,12 +11,13 @@
               priority = 1;
               name = "ESP";
               start = "1M";
-              end = "128M";
+              end = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                esp = true;
               };
             };
             root = {
